9be4fc9d8b3d3756fb4d201f43505ab52c274988
Anton <zanton95@gmail.com>
2020-12-09 05:34:12 +0200

    started js, adding alerts

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,17 @@
 
-if(.topnav) {
-    console.log(21);
 
-}
+window.onload=function(){
+    myFunction('indexpage');
+    };
+
+    window.onload=function(){
+        myFunction('songspage');
+        };
+
+    if ('indexpage') {
+        alert("mainpage");
+    }
+    
+    if ('songspage') {
+        alert("Hello! I am an alert box!!");
+    }
