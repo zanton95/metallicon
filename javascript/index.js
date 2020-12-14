6264638a04058504c3aeecd2e93a2f07cde6d1b9
Anton <zanton95@gmail.com>
2020-12-14 03:53:27 +0200

    having trouble with js on the first assignment

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,23 @@
-
+let bdy = document.getElementById(".indexalert");
+let bdyy = document.getElementById(".songsalert");
 
 window.onload=function(){
     myFunction('indexpage');
     };
 
-    window.onload=function(){
-        myFunction('songspage');
-        };
 
     if ('indexpage') {
         alert("mainpage");
     }
     
+    window.onload=function(){
+        myFunction2('songspage');
+        };
+
     if ('songspage') {
         alert("Hello! I am an alert box!!");
     }
+
+bdy.addEventListener("load", myFunction);
+bdyy.addEventListener("load", myFunction2);
+
