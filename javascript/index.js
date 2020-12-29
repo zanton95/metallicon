c0823fae0b4108d05ea0a9255b5bd360c2309bd3
Anton <zanton95@gmail.com>
2020-12-29 02:26:38 +0200

    stuck on last part of practice question

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 function myFunctionIndex(){
-    alert ("Page name: <Home>");
+    alert (fullSong.Title);
 };
 function myFunctionSongs(){
     alert ("Page name: <Songs>");
@@ -13,6 +13,8 @@ function myFunctionNothingElse(){
 function myFunctionUnforgiven(){
     alert ("Page name: <The Unforgiven II>");
 };
-var songNames = ["Nothing Else Matters", ];
-var songs = [];
+var songNames = ["Enter Sandman", "Nothing Else Matters", "The Unforgiven II"];
 
+var fullSong = {Title:"Enter Sandman", Lyrics:"Say your prayers little one Don't forget, my son To include everyone Tuck you in, warm within Keep you free from sin 'Till the sandman he comes Sleep with one eye open Gripping your pillow tight Exit, light Enter, night Take my hand We're off to never-never land"};
+
+// alert (JSON.stringify(fullSong)); --- safe keeping
