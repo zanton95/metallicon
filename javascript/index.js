ff71c4ae30e5784c0e1a2118c4ce6e907d2043ea
Anton <zanton95@gmail.com>
2021-07-28 05:02:07 +0300

    eventlisteners shortened, working on jquery

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,13 @@
 
-/**
- * Lev general notes:
- *  - Put all the event listener registrations together so it's more clear what events you're listening too (all at the start of the code)
- *  - Do not add two different event listeners for the same event. You currently have two for the DOMContentLoaded. It's better that you control
- *    exactly the order of what happens when an event occurs rather than add more and more listeners. You can have one function that calls the others if need be.
- */
 
-document.addEventListener("DOMContentLoaded", nameAndLyrics);
-document.addEventListener("DOMContentLoaded", pageLoaded);
-document.addEventListener("DOMContentLoaded", takingQueryFromUrl);
+
+document.addEventListener("DOMContentLoaded",() => {
+    nameAndLyrics();
+    pageLoaded();
+    takingQueryFromUrl();
+});
 document.addEventListener('keypress', pressEnter );
+
 
 var songs = [
     {title:"Nothing Else Matters", lyrics:"So close, no matter how far <br> Couldn't be much more from the heart <br> Forever trusting who we are <br> And nothing else matters <br> <br> Never opened myself this way <br> Life is ours, we live it our way <br> All these words I don't just say <br> And nothing else matters <br> <br> Trust I seek and I find in you <br> Every day for us something new <br> Open mind for a different view <br> And nothing else matters <br> <br> Never cared for what they do <br> Never cared for what they know <br> But I know <br> <br> So close, no matter how far <br> Couldn't be much more from the heart <br> Forever trusting who we are <br> And nothing else matters <br> <br> Never cared for what they do <br> Never cared for what they know <br> But I know <br> <br> I never opened myself this way <br> Life is ours, we live it our way <br> All these words I don't just say <br> And nothing else matters <br> <br> Trust I seek and I find in you <br> Every day for us, something new <br> Open mind for a different view <br> And nothing else matters <br> <br> Never cared for what they say <br> Never cared for games they play <br> Never cared for what they do <br> Never cared for what they know <br> And I know, yeah! <br> <br> So close, no matter how far <br> Couldn't be much more from the heart <br> Forever trusting who we are <br> No, nothing else matters <br>" },
@@ -17,9 +15,6 @@ var songs = [
     {title:"The Unforgiven II", lyrics:"Lay beside me and tell me what they've done <br> And speak the words I wanna hear to make my demons run <br> The door is locked now but it's open if you're true <br> If you can understand the me then I can understand the you <br> <br> Lay beside me, under wicked sky <br> Through black of day, dark of night, we share this, paralyzed <br> The door cracks open but there's no sun shining through <br> Black heart scarring darker still but there's no sun shining through <br> No there's no sun shining through, no there's no sun shining <br> <br> What I've felt, what I've known <br> Turn the pages, turn the stone <br> Behind the door, should I open it for you? <br> <br> Yeah, what I've felt, what I've known <br> Sick and tired, I stand alone <br> Could you be there <br> 'Cause I'm the one who waits for you <br> Or are you unforgiven too? <br> <br> Come lay beside me, this won't hurt I swear <br> She loves me not, she loves me still but she'll never love again <br> She lay beside me but she'll be there when I'm gone <br> Black hearts scarring darker still, yes she'll be there when I'm gone <br> Yes she'll be there when I'm gone, dead sure she'll be there? <br> <br> What I've felt, what I've known <br> Turn the pages, turn the stone <br> Behind the door, should I open it for you? <br> <br> Yeah, what I've felt, what I've known <br> Sick and tired, I stand alone <br> Could you be there <br> 'Cause I'm the one who waits for you <br> Or are you unforgiven too? <br> <br> Lay beside me, tell me what I've done <br> The door is closed so are your eyes <br> But now I see the sun, now I see the sun <br> <br> Yes now I see it <br> What I've felt, what I've known <br> Turn the pages, turn the stone <br> Behind the door, should I open it for you <br> <br> Yeah, what I've felt, what I've known <br> So sick and tired, I stand alone <br> Could you be there <br> 'Cause I'm the one who waits <br> The one who waits for you <br> <br> Oh, what I've felt, what I've known <br> Turn the pages, turn the stone <br> Behind the door, should I open it for you? (so I dub thee unforgiven) <br> <br> Oh, what I've felt <br> Oh, what I've known <br> I'll take this key and I'll bury it in you <br> Because you're unforgiven too <br> <br> Never free, never me <br> 'Cause you're unforgiven too, oh oh" },
 ];
 
-// function getFileName (url) {
-//     return url.split("/").pop();
-// }
 
 function nameAndLyrics () {
         var url = document.URL;
@@ -55,18 +50,7 @@ function pageLoaded () {
     singleSong.classList.add("VisibleHtmlElement")
     } else if (document.URL.split("=")[1] === "search-results?p") {
         
-       /**
-         * TODO flow:
-         *  - Take the query text from the url: this is the song name we're looking for.
-         *  - Note: let's start with just looking at EXACTLY that name, and improve the search later on
-         *  - Go over your song list and find a song with that name
-         *  - Alert the found song
-         *  - instead of logging name of song lets add it to a new array called matchedSongs( empty array and add found songs to it)
-         *  - lets implement a better search: 
-         *  - if input text enter sandman is all upper or lower case it should still find songs( search should ignore lower and uppercase letters)
-         *  - now lets match songs even if the user didn't write the full name in other words if a song name contains the input text anywhere in the text then it's a match  
-         */
-
+      
            // ----- remind lev jquery ------ 
 
          /**
@@ -165,40 +149,3 @@ function takingQueryFromUrl() {
 
 
 
-// function pressEnter(event) {
-//     if (event.keyCode !== 13) {
-//         // Return is a special keyword. It tells JS to stop running the current function 
-//         // (pressEnter in this case)
-//         return;
-//     }
-
-//     // The rest will only run if the keyCode is 13.
-//     var inputElement = document.getElementById('search-bar-press');
-//     var searchText = inputElement.value;
-
-//     // Lev & Anton together:
-//     // At this point, we know to check if Enter was pressed. 
-//     // We then take the text from the input and alert it.
-//     // Since we only want to alert if we didn't get an empty value, we need to add an if.
-//     // The if should check if the string we got isn't an empty string AND isn't just white spaces.
-//     // Hint: you can start with just checking for the empty string and then for both
-//     // the empty string and the white spaces.
-
-//     alert(searchText);
-
-//     // sayMyName('Anton', 'Zimin');
-//     // alert('function was called.');
-
-//     // var searchBarInputBox = document.getElementById("search-bar-press")
-//     // var keyCode = (window.event) ? e.which : e.keyCode;
-
-//     // if (searchBarInputBox.value && keyCode == 13) {
-
-//     // }
-
-// } 
-
-// function sayMyName (name, lastName) {
-//     var fullName = name + ' ' + lastName;
-//     alert(fullName);
-// }
